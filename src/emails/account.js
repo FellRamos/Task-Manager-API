1dfd14db29d43f8e0f257f7182315fcc985126a2
FellRamos <feliciano.ramos.pro@gmail.com>
2019-09-01 16:21:38 +0200

    Create Sendwelcomemail and byemail - Chapter15-video3
    Created a function to send email to the user when he create a user
    profile, and another when he deletes it.
    
    Is quite simple, and everything is separated in its own module
    (account.js)

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,22 @@ const sendgridAPIKey = 'SG.YpB4haa6QO-94RhezQfmJw.13fmnEyYAsIJ0Os1FkF7KbVmhMKkTk
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to: 'feliciano.ramos.pro@gmail.com',
-    from: 'feliciano.ramos.pro@gmail.com',
-    subject: 'This is my first email from node.js!',
-    text: 'Some text here..'
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'feliciano.ramos.pro@gmail.com',
+        subject: 'Thanks for joining us!',
+        text: `Welcome to the app, ${name}. Let me know what you think of the app`
+    })
+}
+
+const sendByeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'feliciano.ramos.pro@gmail.com',
+        subject: 'Sorry to see you leaving :(',
+        text: `We are sorry to see you leaving ${name}. Can you please let us know why you are leaving?`
+    })
+}
+
+module.exports = { sendWelcomeEmail, sendByeEmail }
